d0a6ed8d29c90d5661ea76c223ab00fc3f95df1a
Markus Mossig <markus.mossig@imaginarity.com>
2024-04-30 14:17:14 +0200

    initial

---
+++ reactfirebase/src/App.tsx
@@ -0,0 +1,11 @@
+// https://github.com/abdulhakim-altunkaya/youtube_react_firebase_database/blob/main/connection_guide.txt
+
+function App() {
+  return (
+    <div className="App">
+      dfghj dfgh dsf
+    </div>
+  );
+}
+
+export default App;
