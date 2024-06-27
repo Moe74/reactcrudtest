d5ca6b45c46dd6f2c1fb975929f95c16ef022c1e
maya30804 <maya.streicher@outlook.de>
2024-06-27 11:51:02 +0200

    updatee

--- reactfirebase/src/components/Single.tsx
+++ reactfirebase/src/components/Single.tsx
@@ -1,8 +1,7 @@
 import { get, getDatabase, ref } from "firebase/database";
 import _ from "lodash";
-import { Accordion, AccordionTab } from "primereact/accordion";
+import { Accordion as AccordionPR, AccordionTab } from "primereact/accordion";
 import { Button } from "primereact/button";
-import { Card } from "primereact/card";
 import {
   InputNumber,
   InputNumberValueChangeEvent,
@@ -24,6 +23,18 @@ import {
   formatMinuteToHours,
   useElementWidth,
 } from "./Helpers";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Card from "@mui/material/Card";
 
 const HeaderContainer = styled.div<{ imageUrl: string }>`
   width: 100%;
@@ -78,195 +89,228 @@ function Single() {
     </div>
   );
 
-  const footer = (
-    <div>
-      <Button label="go back" onClick={() => navigate(-1)} />
-    </div>
-  );
-
   return (
     <div ref={divRef}>
       {/* <Header /> */}
       {recipe && (
-        <Card
-          title={recipe.title}
-          subTitle={recipe.description}
-          header={header}
-          footer={footer}
-          style={{ marginBottom: 50 }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              alignItems: "center",
-              gap: "20px",
-              marginBottom: 12.5,
-            }}
+        <Card variant="outlined">
+          <CardMedia sx={{ height: 140 }} image={imagePath} />
+          <CardContent
+            /* title={recipe.title}
+            subTitle={recipe.description} */
+            /* header={header}
+            footer={footer} */
+            style={{ marginBottom: 50 }}
+            component="div"
           >
-            <AverageRating firebaseId={firebaseId ?? ""} />
-
-            <Rating
-              value={recipe.difficulty}
-              readOnly
-              cancel={false}
-              onIcon={chefHatActive}
-              stars={3}
-              offIcon={chefHatInactive}
-              style={{ paddingLeft: "30px" }}
-            />
-
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <span
-                style={{ marginRight: 5, paddingLeft: "30px" }}
-                className="pi pi-stopwatch"
-              ></span>
-              <div>{formatMinuteToHours(recipe.duration)} </div>
-            </div>
-
+            <Typography
+              variant="h6"
+              style={{ fontWeight: "bold", marginBottom: 5 }}
+              color="text.primary"
+            >
+              {recipe.title}
+            </Typography>
+            <Typography color="text.secondary" style={{ marginBottom: 10 }}>
+              {recipe.description}
+            </Typography>
             <div
               style={{
                 display: "flex",
+                flexWrap: "wrap",
                 alignItems: "center",
-                color: colors.greenDark,
-                paddingLeft: "35px",
+                gap: "20px",
+                marginBottom: 12.5,
               }}
             >
-              {recipe.isVegi === true && (
-                <>
-                  <span
-                    className="pi pi-apple"
-                    style={{ marginRight: 5, fontWeight: "bolder" }}
-                  />
-                  vegi
-                </>
-              )}
-            </div>
+              <AverageRating firebaseId={firebaseId ?? ""} />
 
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                marginLeft: contentWidth <= 700 ? undefined : "auto",
-              }}
-            >
-              <span style={{ marginRight: 5 }} className="pi pi-users" />
-              <div style={{ position: "relative" }}>
-                <InputNumber
-                  value={personen}
-                  onValueChange={(e: InputNumberValueChangeEvent) =>
-                    handlePersonCount(e.value ?? 5)
-                  }
-                  showButtons
-                  min={1}
-                />
+              <Rating
+                value={recipe.difficulty}
+                readOnly
+                cancel={false}
+                onIcon={chefHatActive}
+                stars={3}
+                offIcon={chefHatInactive}
+                style={{ paddingLeft: "30px" }}
+              />
 
-                {recipe.persons !== personen && (
-                  <Button
-                    icon="pi pi-times"
-                    rounded
-                    size="small"
-                    text
-                    severity="danger"
-                    onClick={handleResetPersonCount}
-                    tooltip="zurücksetzen"
-                    style={{
-                      position: "absolute",
-                      top: 8,
-                      right: 45,
-                      width: 24,
-                      height: 24,
-                    }}
-                  />
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <span
+                  style={{ marginRight: 5, paddingLeft: "30px" }}
+                  className="pi pi-stopwatch"
+                ></span>
+                <div>{formatMinuteToHours(recipe.duration)} </div>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  color: colors.greenDark,
+                  paddingLeft: "35px",
+                }}
+              >
+                {recipe.isVegi === true && (
+                  <>
+                    <span
+                      className="pi pi-apple"
+                      style={{ marginRight: 5, fontWeight: "bolder" }}
+                    />
+                    vegi
+                  </>
                 )}
               </div>
-            </div>
-          </div>
-          <Accordion activeIndex={[0, 1, 2]} multiple={true}>
-            <AccordionTab header="Zutaten">
-              <div
-                style={{
-                  display: "grid",
-                  gridTemplateColumns:
-                    "minmax(150px, max-content) 1fr" /*Geandert Ay */,
-                  columnGap: 10,
-                }}
-              >
-                {_.sortBy(recipe.ingredients, [
-                  (x) => x.amount === undefined || x.unit === undefined,
-                ]).map((x, index) => {
-                  const originalPersons = recipe.persons;
-                  const personsFactor =
-                    (personCount ?? originalPersons) / originalPersons;
-                  const isAmountHidden =
-                    x.amount === undefined || x.unit === undefined;
-                  const temp = x.amount ? x.amount * personsFactor : 0;
-                  const calc = convertUnits(temp, x.unit ?? "");
 
-                  return (
-                    <React.Fragment key={index}>
-                      {!isAmountHidden && (
-                        <div>
-                          <span
-                            style={{
-                              fontSize: 10,
-                              marginRight: 10,
-                              float: "left",
-                              marginTop: 6,
-                            }}
-                            className="pi pi-arrow-right"
-                          />
-                          {Math.round(calc.value)} {calc.unit}
-                        </div>
-                      )}
-                      <div
-                        style={{
-                          gridColumnStart: isAmountHidden ? 1 : 2,
-                          gridColumnEnd: 3,
-                        }}
-                      >
-                        {isAmountHidden ? (
-                          <>
-                            <span
-                              style={{
-                                fontSize: 10,
-                                marginRight: 10,
-                                float: "left",
-                                marginTop: 6,
-                              }}
-                              className="pi pi-arrow-right"
-                            />{" "}
-                            {x.text}
-                          </>
-                        ) : (
-                          x.text
-                        )}
-                      </div>
-                    </React.Fragment>
-                  );
-                })}
-              </div>
-            </AccordionTab>
-            <AccordionTab header="Anleitung">
               <div
                 style={{
-                  display: "grid",
-                  gridTemplateColumns: "max-content 1fr",
-                  columnGap: 10,
-                  rowGap: 5,
+                  display: "flex",
+                  alignItems: "center",
+                  marginLeft: contentWidth <= 700 ? undefined : "auto",
                 }}
               >
-                {_.map(recipe.manual, (m, i) => {
-                  return (
-                    <React.Fragment key={i}>
-                      <div>{i + 1}</div>
-                      <div> {m}</div>
-                    </React.Fragment>
-                  );
-                })}
+                <span style={{ marginRight: 5 }} className="pi pi-users" />
+                <div style={{ position: "relative" }}>
+                  <InputNumber
+                    value={personen}
+                    onValueChange={(e: InputNumberValueChangeEvent) =>
+                      handlePersonCount(e.value ?? 5)
+                    }
+                    showButtons
+                    min={1}
+                  />
+
+                  {recipe.persons !== personen && (
+                    <Button
+                      icon="pi pi-times"
+                      rounded
+                      size="small"
+                      text
+                      severity="danger"
+                      onClick={handleResetPersonCount}
+                      tooltip="zurücksetzen"
+                      style={{
+                        position: "absolute",
+                        top: 8,
+                        right: 45,
+                        width: 24,
+                        height: 24,
+                      }}
+                    />
+                  )}
+                </div>
               </div>
-            </AccordionTab>
-          </Accordion>
+            </div>
+
+            <div style={{ marginBottom: 10 }}>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                >
+                  Zutaten
+                </AccordionSummary>
+                <AccordionDetails>
+                  <div
+                    style={{
+                      display: "grid",
+                      gridTemplateColumns:
+                        "minmax(150px, max-content) 1fr" /*Geandert Ay */,
+                      columnGap: 10,
+                    }}
+                  >
+                    {_.sortBy(recipe.ingredients, [
+                      (x) => x.amount === undefined || x.unit === undefined,
+                    ]).map((x, index) => {
+                      const originalPersons = recipe.persons;
+                      const personsFactor =
+                        (personCount ?? originalPersons) / originalPersons;
+                      const isAmountHidden =
+                        x.amount === undefined || x.unit === undefined;
+                      const temp = x.amount ? x.amount * personsFactor : 0;
+                      const calc = convertUnits(temp, x.unit ?? "");
+
+                      return (
+                        <React.Fragment key={index}>
+                          {!isAmountHidden && (
+                            <div>
+                              <span
+                                style={{
+                                  fontSize: 10,
+                                  marginRight: 10,
+                                  float: "left",
+                                  marginTop: 6,
+                                }}
+                                className="pi pi-arrow-right"
+                              />
+                              {Math.round(calc.value)} {calc.unit}
+                            </div>
+                          )}
+                          <div
+                            style={{
+                              gridColumnStart: isAmountHidden ? 1 : 2,
+                              gridColumnEnd: 3,
+                            }}
+                          >
+                            {isAmountHidden ? (
+                              <>
+                                <span
+                                  style={{
+                                    fontSize: 10,
+                                    marginRight: 10,
+                                    float: "left",
+                                    marginTop: 6,
+                                  }}
+                                  className="pi pi-arrow-right"
+                                />{" "}
+                                {x.text}
+                              </>
+                            ) : (
+                              x.text
+                            )}
+                          </div>
+                        </React.Fragment>
+                      );
+                    })}
+                  </div>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+            <div>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                >
+                  Anleitung
+                </AccordionSummary>
+                <AccordionDetails>
+                  <div
+                    style={{
+                      display: "grid",
+                      gridTemplateColumns: "max-content 1fr",
+                      columnGap: 10,
+                      rowGap: 5,
+                    }}
+                  >
+                    {_.map(recipe.manual, (m, i) => {
+                      return (
+                        <React.Fragment key={i}>
+                          <div>{i + 1}</div>
+                          <div> {m}</div>
+                        </React.Fragment>
+                      );
+                    })}
+                  </div>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+            {/* <AccordionPR activeIndex={[0, 1, 2]} multiple={true}> */}
+          </CardContent>
+          <CardActions>
+            <Button label="go back" onClick={() => navigate(-1)} />
+          </CardActions>
         </Card>
       )}
       {recipe ? (
