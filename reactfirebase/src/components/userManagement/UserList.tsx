a0660d208f980c4d83b472cb6f49cbaaf760cecc
Aytac Ay <aytac.ay@imaginarity.com>
2024-07-01 16:44:17 +0200

    UserListNew

--- reactfirebase/src/components/userManagement/UserList.tsx
+++ reactfirebase/src/components/userManagement/UserList.tsx
@@ -1,54 +1,87 @@
-import React from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
-import { User } from '../UserManagement';
+import React from "react";
+import { User } from "../UserManagement";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import _ from "lodash";
+import ConfirmButton from "../ConfirmButton";
+import EditIcon from "@mui/icons-material/Edit";
 
 interface UserListProps {
-    users: User[];
-    isAdmin: boolean;
-    handleEdit: (user: User) => void;
-    confirmDelete: (id: string) => void;
+  users: User[];
+  isAdmin: boolean;
+  handleEdit: (user: User) => void;
+  confirmDelete: (id: string) => void;
 }
 
-const UserList: React.FC<UserListProps> = ({ users, isAdmin, handleEdit, confirmDelete }) => {
-    const buttons = (user: User) => (
-        <>
-            {isAdmin && (
-                <>
-                    <Button
-                        icon="pi pi-pen-to-square"
-                        severity="warning"
-                        aria-label="Edit"
+const UserList: React.FC<UserListProps> = ({
+  users,
+  isAdmin,
+  handleEdit,
+  confirmDelete,
+}) => {
+  return (
+    <div style={{ width: "100%", gridColumnStart: 1, gridColumnEnd: 4 }}>
+      <h3>User List</h3>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="left">Email</TableCell>
+              <TableCell align="left">Admin</TableCell>
+              {isAdmin && <TableCell align="left">Actions</TableCell>}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {_.map(users, (user) => {
+              const uId = user.id ?? "";
+              return (
+                <TableRow
+                  key={user.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {user.name}
+                  </TableCell>
+                  <TableCell align="left">{user.email}</TableCell>
+                  <TableCell align="left">
+                    {user.userIsAdmin ? "Yes" : "No"}
+                  </TableCell>
+                  {isAdmin && (
+                    <TableCell align="left">
+                      <Button
+                        variant="contained"
+                        startIcon={<EditIcon />}
                         onClick={() => handleEdit(user)}
-                        style={{ float: 'left', marginRight: 5 }}
-                        tooltip='Edit User'
-                        tooltipOptions={{ position: 'left' }}
-                    />
-                </>
-            )}
-            <Button
-                icon="pi pi-trash"
-                severity="danger"
-                onClick={() => confirmDelete(user.id!)}
-                style={{ float: 'left' }}
-                tooltip='Delete User'
-                tooltipOptions={{ position: 'right' }}
-            />
-        </>
-    );
+                        style={{ float: "right" }}
+                        sx={{ ml: 2 }}
+                      >
+                        Edit User
+                      </Button>
 
-    return (
-        <div style={{ width: '100%', gridColumnStart: 1, gridColumnEnd: 4 }}>
-            <h3>User List</h3>
-            <DataTable value={users}>
-                <Column field="name" header="Name" />
-                <Column field="email" header="Email" />
-                <Column field="userIsAdmin" header="Admin" body={(rowData) => (rowData.userIsAdmin ? 'Yes' : 'No')} />
-                {isAdmin && <Column header="Actions" body={buttons} style={{ width: 110 }} />}
-            </DataTable>
-        </div>
-    );
+                      <ConfirmButton
+                        text="Delete"
+                        action={() => confirmDelete(uId)}
+                        style={{ float: "right" }}
+                      />
+                    </TableCell>
+                  )}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default UserList;
