6605f90be697da0de4dbfde798e70fe6620c62eb
maya30804 <maya.streicher@outlook.de>
2024-07-01 17:01:38 +0200

    updatee

--- reactfirebase/src/components/userManagement/UserForm.tsx
+++ reactfirebase/src/components/userManagement/UserForm.tsx
@@ -1,181 +1,198 @@
-import React from 'react';
-import { InputText } from 'primereact/inputtext';
-import { Checkbox } from 'primereact/checkbox';
-import { Button } from 'primereact/button';
+import React from "react";
+import { Button, FormControlLabel, Switch, TextField } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
+import CheckIcon from "@mui/icons-material/Check";
+import ExclamationIcon from "@mui/icons-material/ErrorOutline";
+import PasswordIcon from "@mui/icons-material/Password";
 
 interface UserFormProps {
-    name: string;
-    setName: (name: string) => void;
-    setNameError: (name: string) => void;
-    email: string;
-    setEmail: (email: string) => void;
-    setEmailError: (name: string) => void;
-    password: string;
-    setPassword: (password: string) => void;
-    userIsAdmin: boolean;
-    setUserIsAdmin: (userIsAdmin: boolean) => void;
-    showPassword: boolean;
-    setShowPassword: (showPassword: boolean) => void;
-    handleSubmit: () => void;
-    resetForm: () => void;
-    editId: string | null;
-    isLoading: boolean;
-    nameError: string;
-    emailError: string;
-    setShowChangePasswordDialog: (show: boolean) => void; // Add this prop
+  name: string;
+  setName: (name: string) => void;
+  setNameError: (name: string) => void;
+  email: string;
+  setEmail: (email: string) => void;
+  setEmailError: (name: string) => void;
+  password: string;
+  setPassword: (password: string) => void;
+  userIsAdmin: boolean;
+  setUserIsAdmin: (userIsAdmin: boolean) => void;
+  showPassword: boolean;
+  setShowPassword: (showPassword: boolean) => void;
+  handleSubmit: () => void;
+  resetForm: () => void;
+  editId: string | null;
+  isLoading: boolean;
+  nameError: string;
+  emailError: string;
+  setShowChangePasswordDialog: (show: boolean) => void; // Add this prop
 }
 
 const UserForm: React.FC<UserFormProps> = ({
-    name,
-    setName,
-    email,
-    setEmail,
-    setEmailError,
-    password,
-    setPassword,
-    userIsAdmin,
-    setUserIsAdmin,
-    showPassword,
-    setShowPassword,
-    handleSubmit,
-    resetForm,
-    editId,
-    isLoading,
-    nameError,
-    emailError,
-    setNameError,
-    setShowChangePasswordDialog, // Add this prop
+  name,
+  setName,
+  email,
+  setEmail,
+  setEmailError,
+  password,
+  setPassword,
+  userIsAdmin,
+  setUserIsAdmin,
+  showPassword,
+  setShowPassword,
+  handleSubmit,
+  resetForm,
+  editId,
+  isLoading,
+  nameError,
+  emailError,
+  setNameError,
+  setShowChangePasswordDialog, // Add this prop
 }) => {
-    const saveable = name && email && (password || editId);
+  const saveable = name && email && (password || editId);
 
-    const settingName = (val: string) => {
-        setName(val)
-        setNameError('');
-    };
+  const settingName = (val: string) => {
+    setName(val);
+    setNameError("");
+  };
 
-    const settingEmail = (val: string) => {
-        setEmail(val)
-        setEmailError('');
-    };
+  const settingEmail = (val: string) => {
+    setEmail(val);
+    setEmailError("");
+  };
 
-    return (
+  return (
+    <>
+      <div>Name</div>
+      <div>
+        <TextField
+          id="name"
+          style={{ width: "100%" }}
+          label="Name"
+          value={name}
+          onChange={(e) => settingName(e.target.value)}
+          variant={!name ? "outlined" : undefined}
+          helperText={nameError ?? undefined}
+        />
+      </div>
+      <div>
+        {!name && (
+          <span
+            className="pi pi-exclamation-circle"
+            style={{ color: "#D13438", fontSize: "1.5rem" }}
+          />
+        )}
+      </div>
+      <div>E-Mail</div>
+      <div>
+        <TextField
+          id="email"
+          label="Email"
+          value={email}
+          onChange={(e) => settingEmail(e.target.value)}
+          helperText={emailError ?? undefined}
+          error={emailError !== ""}
+        />
+      </div>
+      <div>
+        {!email && (
+          <span
+            className="pi pi-exclamation-circle"
+            style={{ color: "#D13438", fontSize: "1.5rem" }}
+          />
+        )}
+      </div>
+      {!editId && (
         <>
-            <div>Name</div>
-            <div>
-                <InputText
-                    id="name"
-                    style={{ width: '100%' }}
-                    value={name}
-                    onChange={(e) => settingName(e.target.value)}
-                    invalid={!name}
-                    variant={!name ? 'filled' : undefined}
-                    autoComplete="off"
-                />
-                {nameError && <small style={{ color: 'red' }}>{nameError}</small>}
-            </div>
-            <div>
-                {!name && (
-                    <span className="pi pi-exclamation-circle" style={{ color: '#D13438', fontSize: '1.5rem' }} />
-                )}
-            </div>
-            <div>E-Mail</div>
-            <div>
-                <InputText
-                    id="email"
-                    style={{ width: '100%' }}
-                    value={email}
-                    onChange={(e) => settingEmail(e.target.value)}
-                    invalid={!email}
-                    variant={!email ? 'filled' : undefined}
-                    autoComplete="off"
-                />
-                {emailError && <small style={{ color: 'red' }}>{emailError}</small>}
-            </div>
-            <div>
-                {!email && (
-                    <span className="pi pi-exclamation-circle" style={{ color: '#D13438', fontSize: '1.5rem' }} />
-                )}
-            </div>
-            {!editId && (
-                <>
-                    <div>Password</div>
-                    <div>
-                        <InputText
-                            id="password"
-                            style={{ width: '100%' }}
-                            type={showPassword ? 'text' : 'password'}
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            invalid={!password}
-                            variant={!password ? 'filled' : undefined}
-                            autoComplete="new-password"
-                        />
-                    </div>
-                    <div>
-                        {!password && (
-                            <span className="pi pi-exclamation-circle" style={{ color: '#D13438', fontSize: '1.5rem' }} />
-                        )}
-                    </div>
-                    <div style={{ gridColumnStart: 2, gridColumnEnd: 4 }}>
-                        <Checkbox
-                            inputId="showPassword"
-                            value="showPassword"
-                            onChange={(e) => setShowPassword(!showPassword)}
-                            checked={showPassword}
-                        />
-                        <label style={{ marginLeft: '10px' }} htmlFor="showPassword" className="ml-2">
-                            Passwort anzeigen
-                        </label>
-                    </div>
-                </>
+          <div>Password</div>
+          <div>
+            <TextField
+              id="password"
+              label="Passwort"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              style={{ width: "100%" }}
+              type={showPassword ? "text" : "password"}
+              variant={!password ? "outlined" : undefined}
+            />
+          </div>
+          <div>
+            {!password && (
+              <span
+                className="pi pi-exclamation-circle"
+                style={{ color: "#D13438", fontSize: "1.5rem" }}
+              />
             )}
-            <div>Admin?</div>
-            <div>
-                <div style={{ gridColumnStart: 2, gridColumnEnd: 3 }}>
-                    <div className="p-inputgroup">
-                        <span className="p-inputgroup-addon">
-                            <Checkbox onChange={() => setUserIsAdmin(!userIsAdmin)} checked={userIsAdmin} />
-                        </span>
-                        <InputText
-                            value={userIsAdmin ? 'ja' : 'nein'}
-                            style={{ width: '100%', color: '#323130', pointerEvents: 'none' }}
-                            variant="filled"
-                        />
-                    </div>
-                </div>
-            </div>
-            <div style={{ gridColumnStart: 1, gridColumnEnd: 4, background: '#323130', height: 1 }} />
-            <div style={{ gridColumnStart: 2, gridColumnEnd: 3 }}>
-                <Button
-                    onClick={handleSubmit}
-                    disabled={!saveable}
-                    className="btn"
-                    label={saveable ? 'Submit' : 'MISSING DATA'}
-                    style={{ float: 'right' }}
-                    severity={saveable ? 'success' : 'danger'}
-                    outlined={!saveable}
-                    icon={saveable ? "pi pi-check" : "pi pi-exclamation-circle"}
+          </div>
+          <div style={{ gridColumnStart: 2, gridColumnEnd: 4 }}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={showPassword}
+                  onChange={(e) => setShowPassword(!showPassword)}
                 />
-                <Button
-                    onClick={resetForm}
-                    disabled={isLoading}
-                    className="btn"
-                    label={'Reset'}
-                    style={{ float: 'right', marginRight: '20px' }}
-                    icon="pi pi-times"
-                />
-                {editId && (
-                    <Button
-                        label="Change Password"
-                        severity="info"
-                        onClick={() => setShowChangePasswordDialog(true)}
-                        style={{ float: 'left', marginRight: '20px' }}
-                        icon="pi pi-key"
-                    />
-                )}
-            </div>
+              }
+              label={
+                showPassword ? "Passwort ausblenden" : "Passwort einblenden"
+              }
+            />
+          </div>
         </>
-    );
+      )}
+      <div>Admin?</div>
+      <div>
+        <div style={{ gridColumnStart: 2, gridColumnEnd: 3 }}>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={userIsAdmin}
+                onChange={() => setUserIsAdmin(!userIsAdmin)}
+              />
+            }
+            label={userIsAdmin ? "ja" : "nein"}
+          />
+        </div>
+      </div>
+      <div
+        style={{
+          gridColumnStart: 1,
+          gridColumnEnd: 4,
+          background: "#323130",
+          height: 1,
+        }}
+      />
+      <div style={{ gridColumnStart: 2, gridColumnEnd: 3 }}>
+        <Button
+          onClick={handleSubmit}
+          color={saveable ? "success" : "error"}
+          disabled={!saveable}
+          style={{ float: "right" }}
+          variant={saveable ? "contained" : "outlined"}
+          startIcon={saveable ? <CheckIcon /> : <ExclamationIcon />}
+        >
+          {saveable ? "Submit" : "MISSING DATA"}
+        </Button>
+        <Button
+          startIcon={<ClearIcon />}
+          style={{ float: "right", marginRight: "20px" }}
+          variant="contained"
+          onClick={resetForm}
+        >
+          Reset
+        </Button>
+
+        {editId && (
+          <Button
+            startIcon={<PasswordIcon />}
+            variant="contained"
+            onClick={() => setShowChangePasswordDialog(true)}
+            style={{ float: "left", marginRight: "20px" }}
+          >
+            Change Password
+          </Button>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default UserForm;
