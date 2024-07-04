16f5b30c1c6243abf8407e3009a05f7e236c577b
maya30804 <maya.streicher@outlook.de>
2024-07-04 10:37:10 +0200

    update

--- reactfirebase/src/components/ConfirmButton.tsx
+++ reactfirebase/src/components/ConfirmButton.tsx
@@ -1,66 +1,84 @@
-import CheckIcon from '@mui/icons-material/Check';
-import CancelIcon from '@mui/icons-material/Close';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { Button, ButtonGroup, IconButton, SxProps, Theme } from '@mui/material';
-import * as React from 'react';
+import CheckIcon from "@mui/icons-material/Check";
+import CancelIcon from "@mui/icons-material/Close";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  Button,
+  ButtonGroup,
+  IconButton,
+  SvgIconTypeMap,
+  SxProps,
+  Theme,
+} from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
+import * as React from "react";
 
 interface ConfirmButtonProps {
-    action?: () => void;
-    style?: React.CSSProperties | undefined;
-    text?: string;
-    asIconButton?: boolean;
-    sx?: SxProps<Theme>;
+  action?: () => void;
+  style?: React.CSSProperties | undefined;
+  text?: string;
+  asIconButton?: boolean;
+  sx?: SxProps<Theme>;
+
+  icon?: React.ReactElement;
 }
 
 const ConfirmButton = (p: ConfirmButtonProps) => {
-    const { action, style, text, asIconButton, sx } = p;
-    const [confirmOpen, setconfirmOpen] = React.useState<boolean>(false);
+  const { action, style, text, asIconButton, sx, icon } = p;
+  const [confirmOpen, setconfirmOpen] = React.useState<boolean>(false);
 
-    const onAction = React.useMemo(() => () => {
-        if (action) {
-            setconfirmOpen(false);
-            action();
-        }
-    }, [action]);
+  const onAction = React.useMemo(
+    () => () => {
+      if (action) {
+        setconfirmOpen(false);
+        action();
+      }
+    },
+    [action]
+  );
 
-    return (
+  return (
+    <>
+      {asIconButton ? (
         <>
-            {asIconButton ?
-                <>
-                    <IconButton style={style} sx={sx} onClick={() => setconfirmOpen(!confirmOpen)}>
-                        {confirmOpen ?
-                            <CancelIcon />
-                            :
-                            <DeleteIcon />
-                        }
-                    </IconButton>
-                    {confirmOpen &&
-                        <>
-                            <IconButton style={style} sx={sx} onClick={onAction} color='error'>
-                                <DeleteIcon />
-                            </IconButton>
-                        </>
-                    }
-                </>
-                :
-                <ButtonGroup style={style} sx={sx}>
-                    {confirmOpen &&
-                        <Button variant="contained" color="error" onClick={onAction}>
-                            <CheckIcon sx={{ mr: 1 }} /> yes
-                        </Button>
-                    }
-                    <Button
-                        onClick={() => setconfirmOpen(!confirmOpen)}
-                        color={confirmOpen ? undefined : "error"}
-                        variant={"contained"}
-                        sx={{ background: confirmOpen ? "#000" : undefined }}
-                    >
-                        {!confirmOpen && <DeleteIcon sx={{ mr: text ? 1 : 0 }} />}
-                        {confirmOpen ? "no" : text}
-                    </Button>
-                </ButtonGroup >
-            }
+          <IconButton
+            style={style}
+            sx={sx}
+            onClick={() => setconfirmOpen(!confirmOpen)}
+          >
+            {confirmOpen ? <CancelIcon /> : icon ?? <DeleteIcon />}
+          </IconButton>
+          {confirmOpen && (
+            <>
+              <IconButton
+                style={style}
+                sx={sx}
+                onClick={onAction}
+                color="error"
+              >
+                icon ?? <DeleteIcon />
+              </IconButton>
+            </>
+          )}
         </>
-    );
-}
+      ) : (
+        <ButtonGroup style={style} sx={sx}>
+          {confirmOpen && (
+            <Button variant="contained" color="error" onClick={onAction}>
+              <CheckIcon sx={{ mr: 1 }} /> yes
+            </Button>
+          )}
+          <Button
+            onClick={() => setconfirmOpen(!confirmOpen)}
+            color={confirmOpen ? undefined : "error"}
+            variant={"contained"}
+            sx={{ background: confirmOpen ? "#000" : undefined }}
+          >
+            {!confirmOpen && (icon ?? <DeleteIcon sx={{ mr: text ? 1 : 0 }} />)}
+            {confirmOpen ? "no" : text}
+          </Button>
+        </ButtonGroup>
+      )}
+    </>
+  );
+};
 export default ConfirmButton;
