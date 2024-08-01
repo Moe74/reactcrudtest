492263f4ebf9cb4edb7e469474e00aebdbed6806
Markus Mossig <markus.mossig@imaginarity.com>
2024-08-01 09:49:41 +0200

    aufgabe

---
+++ reactfirebase/src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+// Erstellt eine Komponente Button in dieser Datei.
+// Follgendes soll der Button "vorerst" können.
+
+// Ich möchte folgendes angeben können:
+
+// Rahmenfarbe
+// Hintergrundfarbe
+// Textfarbe
+// Weite
+// Icon
+// Text
+// Linkziel (onClick)
+
+// Bindet einen Button exemplarisch in Spielwiese ein
+
+export { }
